5a67136db6205d65930eba539defa93cf3772b5c
Pavel Vrublevsky <pavel.vrublevsky@corp.solbeg.com>
2016-10-27 13:33:57 +0300

    - fix directive;

--- README.md
+++ README.md
@@ -7,8 +7,9 @@
 1.  Зависимости
 
     Для работы необходима библиотека vue-resource.js
-    Так как предпологается сохранение методом POST с помощью framework-a Laravel, в мета-тегах  должен быть сгенерирован мета-тэг csrf-token
-    Плагин автоматически навешивает на каждый родительский элемент списка обязательную минимальную высоту в 20 px;
+    Так как предпологается сохранение методом POST с помощью framework-a Laravel, в глобальные настройки должен быть передан csrf-token Laravel
+    В случае, если в родительском элементе нету сортируемых элемнтов, его высота будет равно 0px, и передать в такое элемент сортируемый объект будет невозможно, 
+    поэтому всем родительским элементам обязательно надо навешивать минимальную высоту ( по умолчанию плагин выставставит minHeight = 20 px).
 
 2.  Подключить скрипт на страницу, где необходима сортировка
    
@@ -20,7 +21,7 @@
     В рамках одного сортируемого списка структура следующая 
 
     ```html 
-            <parent id="parent_id" v-sortable="{global:sortableOptions,local:parent.options}">
+            <parent id="parent_id" v-sortable="{global:sortableOptions,local:parent.options,callback:callback}">
                  <child id="child_id" />
                  <child id="child_id" />
                  <child id="child_id" />
@@ -32,7 +33,7 @@
     ```html 
             <div v-for="board in boards" >
                 <p>{{board.name}}</p>
-                  <ul v-bind:id="board.id" v-sortable="{global:sortableOptions,local:board.options}">
+                  <ul v-bind:id="board.id" v-sortable="{global:sortableOptions,local:board.options,callback:callback}">
                     <li v-for="item in board.list"  v-bind:id="item.id"><p>{{item.name}}</p></li>
                 </ul>
             </div>   
@@ -43,7 +44,7 @@
     ```html 
             <div v-for="board in boards" >
                 <p>{{board.name}}</p>
-                <div v-bind:id="board.id" v-sortable="{global:sortableOptions,local:board.options}">
+                <div v-bind:id="board.id" v-sortable="{global:sortableOptions,local:board.options,callback:callback}">
                     <div v-for="item in board.list" v-bind:id="item.id"><p>{{item.name}}</p></div>
                 </div>
             </div>   
@@ -51,10 +52,12 @@
 
 4.  Плагин поддерживает инициализацию некоторыми настройками базового плагина RubaXa/Sortable + ему необходимы свои глобальные настройки.
     Так как настройки каждого сортируемого списка могут отличаться друг от друга, глобально инициализировать эти настройки и передевать
-    в каждый из сортируемых списков не совсем хорошо. В примере выше предложено использовать json объект с разделениями настроек. 
+    в каждый из сортируемых списков не совсем хорошо. В примере выше предложено использовать json объект с разделениями настроек.
+    Также в настройки можно передать callback функцию, которая выполняется после удачного выполнения post-запроса. Вкачестве аргумента принимаются
+    сформированные json-данные запроса. 
    
     ```html 
-         v-sortable="{global:glopalOptions,local:board.options}"
+         v-sortable="{global:glopalOptions,local:board.options,callback:callback}"
     ```
 
     Глобальные настройки
@@ -63,13 +66,17 @@
         {
             groupIdAttribute: 'id',
             itemIdAttribute: 'id',
-            urlSaveAction: '/index1.php'
+            urlSaveAction: '/index1.php',
+            csrfToken:'sfsd4353w345',
+            minHeigthElement:100,
         }
     ```
     где 
     - **groupIdAttribute** - название атрибута родительского элемента,уникальный идентификатор сортируемой группы, по умолчанию - id;
     - **itemIdAttribute**  - название атрибута сортируемого элемента,уникальный идентификатор сортируемого элемента,по умолчанию - id;
     - **urlSaveAction** - алиас веб-роута, куда будет уходить POST запрос с данными.
+    - **csrfToken** - обяхательное значение csrfToken-а Laravel для post-запроса.
+    - **minHeigthElement** - минимальная высота элемента к которому применяеться директива.
 
     Локальные настройки сортируемого списка
 
@@ -86,6 +93,14 @@
     - **disabled**  - сортировка как внутри списка так и во вне не происходит,по умолчанию - false;
     - **handle**  - селектор элемента , за который  происходит сортировка,по умолчанию - срабатывает на всем элементе;
     - **ghostClass** - дополнительный класс, который вешается на плейсхолдер при перетаскивании элемента, по умолчанию - 'item-placeholder'.
+
+    Callback-функций 
+
+    ```javascript 
+        callback:function(data){
+            console.log(data);
+        }
+    ```
 
 5.  После успешного события сортировки происходит событие которое иницирует POST запрос по заданному в настройках url в формате "Request Payload".
 
@@ -106,7 +121,7 @@
     - **new_group_id** - уникальный идентификатор группы в которую был перемещен сортируемый элемент;
     - **_token** - секретный токен, валидируемый Laravel в Post запросах.
 
-    В случае успешного сохранения данных , ничего не происходит, работа плагина продолжается. 
+    В случае успешного сохранения данных , происходит вызов callback функции , если был передан callable - обект , иначе ничего не происходит, работа плагина продолжается. 
     В случае ошибки на сервере в теле ответа ожидается json-object с текстом в ключе message
     ```javascript
        {error: true, message: "Sorting saving error"}
--- src/sortable.js
+++ src/sortable.js
@@ -1,72 +1,70 @@
-// required Binding attributes
-// boardAtribute - 
-
-
-
-var VueSortableHelper = (function (Vue) {
+;
+(function (Vue, Sortable) {
     if (!Vue.http) {
         throw new Error("Error directive usage : lib vue-resource.js is required");
     }
-    return {
-        CSRF_TOKEN: (function () {
-            var x, myname, inputs = document.getElementsByTagName("meta");
-            for (x = 0; x < inputs.length; x++) {
-                myname = inputs[x].getAttribute("name");
-                if (myname && myname.indexOf("csrf-token") === 0) {
-                    return inputs[x].content;
-                }
-            }
-            throw new Error("Error application : Laravel csrf-token not found");
-        })(),
-        sendNewStatePostRequest: function (url, itemId, orderIndex, oldGroupId, newGroupId) {
-            Vue.http.post(url, {
-                item_id: itemId,
-                order_index: orderIndex,
-                old_group_id: oldGroupId,
-                new_group_id: newGroupId,
-                _token: this.CSRF_TOKEN
-            }, {body: 'JSON'}).then(function (response) {
-
-            }, function (response) {
-                throw new Error("Error application : " + response.body.message);
-            });
-        },
-        mergeOptions: function (obj1, obj2) {
-            var obj3 = {};
-            for (var attrname in obj1) {
-                obj3[attrname] = obj1[attrname];
-            }
-            for (var attrname in obj2) {
-                obj3[attrname] = obj2[attrname];
-            }
-            return obj3;
-        }
-    };
-})(Vue);
-
-Vue.directive('sortable', function (el, binding) {
-    el.style.minHeight = '20px';
-    if (!binding.value.global && !binding.value.global.urlSaveAction) {
-       throw new Error("Error directive usage : save-url-alias not set"); 
-    }
-    var itemIdAttribute=binding.value.global.itemIdAttribute?binding.value.global.itemIdAttribute:'id',
-        groupIdAttribute=binding.value.global.groupIdAttribute?binding.value.global.groupIdAttribute:'id';
-    var options = {
-        group: {name: 'sortable', put: true, pull: true},
-        sort: true,
-        disabled: false,
-        handle: false,
-        ghostClass: 'item-placeholder',
-        onEnd: function (evt) {
-            var url = binding.value.global.urlSaveAction,
-                    itemId = parseInt(evt.item.getAttribute(itemIdAttribute)),
-                    orderIndex = evt.newIndex,
-                    oldGroupId = parseInt(evt.from.getAttribute(groupIdAttribute)),
-                    newGroupId = parseInt(evt.item.parentElement.getAttribute(groupIdAttribute));
-            VueSortableHelper.sendNewStatePostRequest(url, itemId, orderIndex, oldGroupId, newGroupId);
-        }
-    }, mergedOptions;
-    mergedOptions=VueSortableHelper.mergeOptions(options,binding.value.local);
     
-    Sortable.create(el, mergedOptions);
-});
+    var VueSortableHelper = (function (Vue) {
+        return {
+            sendNewStatePostRequest: function (url, postData, callback) {
+                Vue.http.post(url, postData, {body: 'JSON'}).then(function (response) {
+                    if (callback) {
+                        callback(response.data);
+                    }
+                }, function (response) {
+                    throw new Error("Error application : " + response.body.message);
+                });
+            },
+            mergeOptions: function (obj1, obj2) {
+                var obj3 = {};
+                for (var attrname in obj1) {
+                    obj3[attrname] = obj1[attrname];
+                }
+                for (var attrname in obj2) {
+                    obj3[attrname] = obj2[attrname];
+                }
+                return obj3;
+            }
+        };
+    })(Vue);
+
+    Vue.directive('sortable', function (el, binding) {
+        el.style.minHeight = binding.value.global.minHeigthElement?binding.value.global.minHeigthElement+'px':'20px';
+        if (!binding.value.global && !binding.value.global.urlSaveAction) {
+            throw new Error("Error directive usage : save-url-alias not set");
+        }
+        if (!binding.value.global && !binding.value.global.csrfToken) {
+            throw new Error("Error directive usage : csrf_token not set");
+        }
+        var sorted, itemIdAttribute = binding.value.global.itemIdAttribute ? binding.value.global.itemIdAttribute : 'id',
+                groupIdAttribute = binding.value.global.groupIdAttribute ? binding.value.global.groupIdAttribute : 'id';
+        var options = {
+            group: {name: 'sortable', put: true, pull: true},
+            sort: true,
+            disabled: false,
+            handle: false,
+            ghostClass: 'item-placeholder',
+            onEnd: function (evt) {
+                var url = binding.value.global.urlSaveAction,
+                        postData = {
+                            item_id: parseInt(evt.item.getAttribute(itemIdAttribute)),
+                            order_index: evt.newIndex,
+                            old_group_id: parseInt(evt.from.getAttribute(groupIdAttribute)),
+                            new_group_id: parseInt(evt.item.parentElement.getAttribute(groupIdAttribute)),
+                            _token: binding.value.global.csrfToken
+                        };
+                VueSortableHelper.sendNewStatePostRequest(url, postData, binding.value.callback);
+            },
+            onMove: function (evt) {
+                if (sorted) {
+                    sorted.parentNode.className = sorted.parentNode.className.replace('hover', '');
+                }
+                evt.to.parentElement.className = evt.to.parentElement.className + ' hover';
+                sorted = evt.to;
+            }
+        }, mergedOptions;
+        mergedOptions = VueSortableHelper.mergeOptions(options, binding.value.local);
+
+        Sortable.create(el, mergedOptions);
+    });
+})(Vue, Sortable);
